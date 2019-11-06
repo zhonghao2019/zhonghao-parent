0722745b672d1ed5c721db1d2b71363213f7c607
中豪 <zhonghao2019@aliyun.com>
2019-11-06 23:48:48 +0800

    异常枚举

--- zh-common/src/main/java/com/zhonghao/common/enums/ExceptionEnum.java
+++ zh-common/src/main/java/com/zhonghao/common/enums/ExceptionEnum.java
@@ -9,7 +9,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 // 异常枚举
 public enum ExceptionEnum {
-    PRICE_CANNOT_BE_NULL(400, "价格不能为空");
+    PRICE_CANNOT_BE_NULL(400, "价格不能为空"),
+    CATEGORY_NOT_FOUND(404, "商品分类为空")
+    ;
 
     private int code;
     private String msg;
