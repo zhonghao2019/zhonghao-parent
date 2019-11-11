01a4e10e54422c6eef74ef2103fbd0630fe07c3a
中豪 <zhonghao2019@aliyun.com>
2019-11-11 21:39:46 +0800

    通用服务

--- zh-common/src/main/java/com/zhonghao/common/enums/ExceptionEnum.java
+++ zh-common/src/main/java/com/zhonghao/common/enums/ExceptionEnum.java
@@ -11,7 +11,8 @@ import lombok.NoArgsConstructor;
 public enum ExceptionEnum {
     PRICE_CANNOT_BE_NULL(400, "价格不能为空"),
     CATEGORY_NOT_FOUND(404, "商品分类为空"),
-    BRAND_NOT_FOUND(404, "品牌没有找到")
+    BRAND_NOT_FOUND(404, "品牌没有找到"),
+    BRAND_SAVE_ERROR(500, "品牌新增失败")
     ;
 
     private int code;
