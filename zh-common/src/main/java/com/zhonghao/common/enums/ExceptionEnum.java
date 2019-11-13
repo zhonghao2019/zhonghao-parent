3dc270df591cc555197036079ec619a42e5e779f
中豪 <zhonghao2019@aliyun.com>
2019-11-13 23:11:45 +0800

    通用服务

--- zh-common/src/main/java/com/zhonghao/common/enums/ExceptionEnum.java
+++ zh-common/src/main/java/com/zhonghao/common/enums/ExceptionEnum.java
@@ -12,7 +12,8 @@ public enum ExceptionEnum {
     PRICE_CANNOT_BE_NULL(400, "价格不能为空"),
     CATEGORY_NOT_FOUND(404, "商品分类为空"),
     BRAND_NOT_FOUND(404, "品牌没有找到"),
-    BRAND_SAVE_ERROR(500, "品牌新增失败")
+    BRAND_SAVE_ERROR(500, "品牌新增失败"),
+    SPEC_GROUP_NOT_FOUND(404, "规格组没有找到")
     ;
 
     private int code;
